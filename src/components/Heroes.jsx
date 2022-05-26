6c68c761c8c439141b2512f1d21671edbddb97a0
Craig <indigomx9@gmail.com>
2022-05-26 14:01:37 -0400

    Setup useEffect for data.

--- src/components/Heroes.jsx
+++ src/components/Heroes.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 
 export const Heroes = () => {
+    const [isLoading, setIsLoading] = React.useState(true);
+    const [data, setData] = React.useState([]);
+
+    const URL = "http://localhost:5000/superheroes";
+    React.useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch(URL);
+            if (!res.ok) throw new Error(res.statusText);
+            const data = await res.json();
+            setData(data);
+            return [...data];
+        };
+        fetchData();        
+        setIsLoading(false);
+    }, []);
+
+    if (isLoading) return <h1>Loading...</h1>
+
     return (
         <React.Fragment>
             <h1>Heroes</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui aliquid quisquam est ipsam corporis sequi quod. Molestias veniam voluptatum odit quia iste sunt commodi. Ab velit qui vel sunt voluptates.</p>
+            {data.map((hero) => (
+                <main key={hero.id}>
+                    <h3>{hero.name}</h3>
+                    <p>{hero.alterEgo}</p>
+                </main>
+            ))}
         </React.Fragment>
     );
 };
